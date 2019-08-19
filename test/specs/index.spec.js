d37a9243429ef1774b29a48c6d0c90413a5f6e6d
Kacper Ochmański <soownic@gmail.com>
2019-08-20 00:54:34 +0200

    Update dependencies (#3)
    * fix: update dependencies and use new svelte compiler
    
    * fix: migrate tests to new svelte version
    
    * chore: revert package version and update peer dependencies

--- test/specs/index.spec.js
+++ test/specs/index.spec.js
@@ -2,40 +2,32 @@ import Test from '../fixtures/Test.html'
 
 describe('Svelte Jest', () => {
   it('should import Svelte component', () => {
-    const el = document.createElement('div')
-    new Test({
-      target: el
-    })
-    expect(el.textContent).toBe('Hello World')
+    const target = document.createElement('div')
+
+    new Test({ target })
+
+    expect(target.textContent).toBe('Hello World')
   })
 
   it('should update the dom', () => {
-    const el = document.createElement('div')
-    const test = new Test({
-      target: el
-    })
+    const target = document.createElement('div')
+    const test = new Test({ target, accessors: true })
 
-    expect(el.textContent).toBe('Hello World')
+    expect(target.textContent).toBe('Hello World')
 
-    test.set({
-      message: 'Test'
-    })
+    test.message = 'Test'
 
-    expect(el.textContent).toBe('Hello Test')
+    expect(target.textContent).toBe('Hello Test')
   })
 
   it('should be Svelte instance', () => {
-    const el = document.createElement('div')
-    const test = new Test({
-      target: el
-    })
+    const target = document.createElement('div')
+    const test = new Test({ target })
 
-    expect(test.get().message).toBe('World')
+    expect(test.message).toBe('World')
 
-    test.set({
-      message: 'Test'
-    })
+    test.message = 'Test'
 
-    expect(test.get().message).toBe('Test')
+    expect(test.message).toBe('Test')
   })
 })
